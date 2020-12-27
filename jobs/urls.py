b36b884aae7372d0908a04e75a3cc6a29a383606
510588264@qq.com <510588264@qq.com>
2020-12-27 22:15:04 +0800

    申请者进入简历列表在线投递简历，（使用django自带的Createview），并附带上一页面信息

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url
+from django.urls import path
 from jobs import views
 
 urlpatterns = [
     # 职位列表
     url(r"^joblist/", views.joblist, name="joblist"),
+    # 职位详情
     url(r"^job/(?P<job_id>\d+)/$", views.detail, name="detail"),
 
     # 首页自动跳转到 职位列表
     url(r"^$", views.joblist, name="name"),
+
+    # 提交简历
+    path('resume/add/', views.ResumeCreateView.as_view(), name='resume-add')
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
-from jobs.models import Job
+from django.http import HttpResponseRedirect
+from jobs.models import Job, Resume
 from jobs.models import Citys,JobTypes
 
 def joblist(request):
@@ -26,3 +28,27 @@ def detail(request, job_id):
         raise Http404("Job does not exist.")
 
     return render(request, 'job.html', {'job': job})
+
+
+class ResumeCreateView(LoginRequiredMixin, CreateView):
+    """    简历职位页面  """
+    template_name = 'resume_form.html'
+    success_url = '/joblist/'
+    model = Resume
+    fields = ["username", "city", "phone",
+        "email", "apply_position", "gender",
+        "bachelor_school", "master_school", "major", "degree", "picture", "attachment",
+        "candidate_introduction", "work_experience", "project_experience"]
+
+    ### 从 URL 请求参数带入默认值
+    def get_initial(self):
+        initial = {}
+        for x in self.request.GET:
+            initial[x] = self.request.GET[x]
+        return initial
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.applicant = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
