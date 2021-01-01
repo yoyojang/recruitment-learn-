af71ef7d7792ee70ab5043047166ceaa1a4a015a
510588264@qq.com <510588264@qq.com>
2021-01-01 16:32:03 +0800

    将申请人信息复制到面试列表，打通两个表。并增加操作，已经django 通知功能

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from jobs.models import Job, Resume
-# Register your models here.
+
+from django.contrib import messages
+from interview.models import Candidate
+from datetime import datetime
+
+def enter_interview_process(modeladmin, request, queryset):
+    candidate_names = ""
+    for resume in queryset:
+        candidate = Candidate()
+        # 把 obj 对象中的所有属性拷贝到 candidate 对象中:
+        candidate.__dict__.update(resume.__dict__)
+        candidate.created_date = datetime.now()
+        candidate.modified_date = datetime.now()
+        candidate_names = candidate.username + "," + candidate_names
+        candidate.creator = request.user.username
+        candidate.save()
+    messages.add_message(request, messages.INFO, '候选人: %s 已成功进入面试流程' % (candidate_names) )
+
+enter_interview_process.short_description = u"进入面试流程"
 
 class JobAdmin(admin.ModelAdmin):
     exclude = ('creator','create_date','modified_date')
@@ -11,6 +29,8 @@ class JobAdmin(admin.ModelAdmin):
         super().save_model(request,obj,form,change)
 
 class ResumeAdmin(admin.ModelAdmin):
+
+    actions = (enter_interview_process,)
 
     list_display = ('username', 'applicant', 'city', 'apply_position', 'bachelor_school', 'master_school', 'major','created_date')
 
