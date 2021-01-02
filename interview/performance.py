452383c6cfd3c2040c3c7517b721ae0b9d46d032
510588264@qq.com <510588264@qq.com>
2021-01-02 16:05:44 +0800

    自定义中间件，记录页面访问时间，并记录到日志

---
+++ interview/performance.py
@@ -0,0 +1,15 @@
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+def performance_logger_middleware(get_response):
+    def middleware(request):
+        start_time = time.time()
+        response = get_response(request)
+        duration = time.time() - start_time
+        response["x-Page-Duration-ms"] = int(duration * 1000)
+        logger.info("%s %s %s", duration, request.path, request.GET.dict())
+        return response
+
+    return middleware
